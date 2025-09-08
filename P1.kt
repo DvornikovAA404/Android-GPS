149a17db07d543b28fdedeab8006d32add221b28
DvornikovAA404 <advornikov0809@gmail.com>
2025-09-08 11:24:17 +0700

    base

---
+++ P1.kt
@@ -0,0 +1,15 @@
+Class Human(
+    var FIO: String,
+    var Age: Int,
+    var Speed: Double
+) {
+    contructor() : this("", 0, 0.0)
+
+    fun move(){
+
+    }
+}
+
+fun main() {
+    println("Hello, Kotlin!")
+}
